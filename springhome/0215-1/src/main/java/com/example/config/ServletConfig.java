67908b2fdb20fe0ca8fb6e7b1ec27c9bfdfb60c0
alisyabob <48196352+ssj9398@users.noreply.github.com>
2020-02-15 11:01:04 +0900

    Add files via upload

---
+++ springhome/0215-1/src/main/java/com/example/config/ServletConfig.java
@@ -0,0 +1,29 @@
+package com.example.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"com.example"})
+public class ServletConfig implements WebMvcConfigurer{
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").
+						addResourceLocations("/resources/");
+	}
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		registry.viewResolver(bean);
+	}
+}
---
+++ springhome/0215-1/src/main/java/com/example/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class [] {  RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		//return new Class [] { ServletConfig.class };
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		//return new String [] { "/" };
+		return null;
+	}
+
+}
